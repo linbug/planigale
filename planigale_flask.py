3cb53fe33a4e47ccd2e9c52b79e107084ca2936f
Lin Taylor <taylorxlin@gmail.com>
2015-12-10 11:28:20 -0500

    Works with refactored Planigale.py; gets form submission answer

--- planigale_flask.py
+++ planigale_flask.py
@@ -1,19 +1,23 @@
-from flask import Flask
-from flask import render_template
-from planigale import Question, Species, load_data
+from flask import Flask, render_template, request, redirect
+from planigale import Question, Species, Planigale, PlanigaleGame, PlanigaleConsole
 
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
         question = Question(data)
+
+
+        if request.method == 'POST':
+            entered_answer = request.form["choice"]
+            print(entered_answer)
 
         return render_template('Game.html',
                            question = question)
 
-
 if __name__ == '__main__':
-    data = load_data()
+    data = Planigale.load_species()
 
     app.run(debug = True)
