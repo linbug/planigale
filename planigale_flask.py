1a44dafffe74a16772f78a8ea890f54fac7e1b6f
Lin Taylor <taylorxlin@gmail.com>
2015-12-10 12:36:29 -0500

    Uses sessions (break with JSON error)

--- planigale_flask.py
+++ planigale_flask.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from planigale import Question, Species, Planigale, PlanigaleGame, PlanigaleConsole
-
+import os
 
 app = Flask(__name__)
+app.secret_key =  os.urandom(24)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-        question = Question(data)
 
+        if "current_game" not in session:
+            session["current_game"] = PlanigaleGame(data)
+            current_question = session["current_game"].questions[0]
+        #question = Question(data)
 
         if request.method == 'POST':
             entered_answer = request.form["choice"]
             print(entered_answer)
 
         return render_template('Game.html',
-                           question = question)
+                           question = current_question )
 
 if __name__ == '__main__':
     data = Planigale.load_species()
